12f8a99a91ff8538f910807864e21ab9db7eca3e
dorfohm <dorfohm@hotmail.com>
2022-09-05 20:35:25 -0300

    api CRUD inicial criado

--- api/src/index.js
+++ api/src/index.js
@@ -7,14 +7,38 @@ const customers = [
     {id: 3, name: 'José Francisco'}
 ];
 
+app.use(express.json());
+
 app.get('/api/ping', (request, response) => {
     response.send({
         message: "pong"
-    })
+    });
 });
 
 app.get('/api/customers', (request, response) => {
     response.send(customers);
+});
+
+app.post('/api/customers', (request, response) => {
+    const { body } = request;
+    customers.push(body);
+    response.send(customers);
+});
+
+app.put('/api/customers/:id', (request, response) => {
+    const { id } = request.params;
+    const { body } = request;
+
+    const index = customers.findIndex(customer => customer.id == id);
+    customers[index] = body;
+
+    response.send(customers);
+});
+
+app.delete('/api/customers/:id', (request, response) => {
+    const { id } = request.params;
+    const newArray = customers.filter(customer => customer.id != id);
+    response.send(newArray);
 });
 
 app.listen(8000, () => {

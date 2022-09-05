296a1893ca30d0c317b3f597baa866a2670d57ac
dorfohm <dorfohm@hotmail.com>
2022-09-05 12:13:19 -0300

    criando endpoint customers

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,22 @@
 import express from 'express';
 const app = express();
 
+const customers = [
+    {id: 1, name: 'Marcos Vinicius'},
+    {id: 2, name: 'Maria Eduarda'},
+    {id: 3, name: 'José Francisco'}
+];
+
 app.get('/api/ping', (request, response) => {
     response.send({
         message: "pong"
     })
-})
+});
+
+app.get('/api/customers', (request, response) => {
+    response.send(customers);
+});
 
 app.listen(8000, () => {
     console.log("Servidor funcionando na porta 8000... ")
-})
+});

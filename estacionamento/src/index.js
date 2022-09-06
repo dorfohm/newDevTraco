ff5ef1f88029796ec46b9645b4dcd313a41a3de0
dorfohm <dorfohm@hotmail.com>
2022-09-06 16:18:05 -0300

    API CRUD finalizado

--- estacionamento/src/index.js
+++ estacionamento/src/index.js
@@ -23,20 +23,72 @@ app.get('/api/vehicles', async (request, response) => {
 
 });
 
-app.post('/api/vehicles/:id', async (request, response) => {
+app.post('/api/vehicles', async (request, response) => {
+    const { model, label, type, owner, observation } = request.body;
     const db = await openDatabase();
-    const vehicles = await db.all(`
-        SELECT * FROM vehicles
-    `)
+    const data = await db.run(`
+        INSERT INTO vehicles(model, label, type, owner, observation)
+        VALUES (?, ?, ?, ?, ?)
+    `, [model, label, type, owner, observation])
     db.close();
+    response.send({
+        id: data.lastID,
+        model,
+        label,
+        type, 
+        owner,
+        observation
+    });
 });
 
-app.put('/api/vehicles/:id', (request, response) => {
+app.put('/api/vehicles/:id', async(request, response) => {
+    const { model, label, type, owner, observation } = request.body;
+    const {id} = request.params;
 
+    const db = await openDatabase();
+
+    const vehicle = await db.get(`
+    SELECT * FROM vehicles WHERE id = ? 
+`, [id]);
+    if(vehicle) {
+        const data = await db.run(`
+        UPDATE vehicles
+            SET model = ?,
+                label = ?,
+                type = ?,
+                owner = ?,
+                observation = ?
+        WHERE id = ?
+        `, [model, label, type, owner, observation, id]);
+    
+        db.close();
+        response.send({
+            id,
+            model,
+            label,
+            type, 
+            owner,
+            observation
+            });
+        return;
+    }
+
+    db.close();
+    response.send(vehicle || {});
 });
 
-app.delete('/api/vehicles/:id', (request, response) => {
-
+app.delete('/api/vehicles/:id', async(request, response) => {
+    const { id } = request.params;
+    const db = await openDatabase();
+    const data = await db.run(`
+        DELETE FROM vehicles 
+        WHERE id = ?
+    `, [id])
+    db.close();
+    response.send({
+        id,
+        message: `Veículo [${id}] removido com sucesso`
+    });
 });
 
 app.listen(8000, () => {

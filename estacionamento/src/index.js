e97152f3d9d858eba3c1a31b138b7ca74c93789c
dorfohm <dorfohm@hotmail.com>
2022-09-06 17:49:30 -0300

    Refatorado o código, separando por arquivos

--- estacionamento/src/index.js
+++ estacionamento/src/index.js
@@ -1,4 +1,6 @@
 import express, { response } from 'express';
+import { checkIn, checkOut, deleteActivity, listActivities } from './controllers/activitiesController.js';
+import { deleteVehicles, insertVehicle, listVehicles, updateVehicles } from './controllers/vehiclesController.js';
 import { openDatabase } from './database.js';
 
 const app = express();
@@ -12,191 +14,16 @@ app.get('/api/ping', (request, response) => {
 });
 
 /* Endpoints Vehicles */
-app.get('/api/vehicles', async (request, response) => {
-
-    const db = await openDatabase();
-    const vehicles = await db.all(`
-        SELECT * FROM vehicles
-    `)
-    db.close();
-    response.send(vehicles)
-
-});
-
-app.post('/api/vehicles', async (request, response) => {
-    const { model, label, type, owner, observation } = request.body;
-    const db = await openDatabase();
-    const data = await db.run(`
-        INSERT INTO vehicles(model, label, type, owner, observation)
-        VALUES (?, ?, ?, ?, ?)
-    `, [model, label, type, owner, observation])
-    db.close();
-    response.send({
-        id: data.lastID,
-        model,
-        label,
-        type, 
-        owner,
-        observation
-    });
-});
-
-app.put('/api/vehicles/:id', async(request, response) => {
-    const { model, label, type, owner, observation } = request.body;
-    const {id} = request.params;
-
-    const db = await openDatabase();
-
-    const vehicle = await db.get(`
-    SELECT * FROM vehicles WHERE id = ? 
-    `, [id]);
-    if(vehicle) {
-        const data = await db.run(`
-        UPDATE vehicles
-            SET model = ?,
-                label = ?,
-                type = ?,
-                owner = ?,
-                observation = ?
-        WHERE id = ?
-        `, [model, label, type, owner, observation, id]);
-    
-        db.close();
-        response.send({
-            id,
-            model,
-            label,
-            type, 
-            owner,
-            observation
-            });
-        return;
-    }
-
-    db.close();
-    response.send(vehicle || {});
-});
-
-app.delete('/api/vehicles/:id', async(request, response) => {
-    const { id } = request.params;
-    const db = await openDatabase();
-    const data = await db.run(`
-        DELETE FROM vehicles 
-        WHERE id = ?
-    `, [id])
-    db.close();
-    response.send({
-        id,
-        message: `Veículo [${id}] removido com sucesso`
-    });
-});
+app.get('/api/vehicles', listVehicles);
+app.post('/api/vehicles', insertVehicle);
+app.put('/api/vehicles/:id', updateVehicles);
+app.delete('/api/vehicles/:id', deleteVehicles);
 
 //Endpoints de activities
-app.post('/api/activities/checkin', async (request, response) => {
-    const { label } = request.body; 
-
-    const db = await openDatabase();
-
-    const vehicle = await db.get(`
-    SELECT * FROM vehicles WHERE label = ? 
-    `, [label]);
-
-    if (vehicle) {
-        const checkinAt = (new Date()).getTime();
-        const data = await db.run(`
-        INSERT INTO activities (vehicle_id, checkin_at)
-        VALUES (?, ?)
-        `, [vehicle.id, checkinAt]);
-        db.close();
-        response.send({
-        vehicle_id: vehicle.id,
-        checkin_at: checkinAt,
-        message: `Veículo [${vehicle.label}] entrou no estacionamento`
-        });
-        return;
-    }
-
-    db.close();
-    response.send({
-        message: `Veículo [${label}] não cadastrado`
-    });
-});
-
-app.put('/api/activities/checkout', async (request, response) => {
-    const { label, price } = request.body; 
-
-    const db = await openDatabase();
-
-    const vehicle = await db.get(`
-    SELECT * FROM vehicles WHERE label = ? 
-    `, [label]);
-
-    if (vehicle) {
-
-        const activityOpen = await db.get(`
-            SELECT * 
-            FROM activities 
-            WHERE vehicle_id = ? 
-            AND checkout_at IS NULL
-        `, [vehicle.id]);
-
-        if  (activityOpen) {
-            const checkoutAt = (new Date()).getTime();
-            const data = await db.run(`
-            UPDATE activities 
-            SET checkout_at = ?,
-                price = ?
-            WHERE id = ?
-            `, [checkoutAt, price, activityOpen.id]);
-
-            db.close();
-            response.send({
-            vehicle_id: vehicle.id,
-            checkout_at: checkoutAt,
-            price: price,
-            message: `Veículo [${vehicle.label}] saiu do estacionamento`
-            });
-            return;
-        }
-
-        db.close();
-        response.send({
-        message: `Veículo [${label}] não realizou nenhum check-in`
-        });
-        return;
-    
-    }
-
-    db.close();
-    response.send({
-        message: `Veículo [${label}] não cadastrado`
-    });
-});
-
-app.delete('/api/activities/:id', async (request, response) => {
-    const { id } = request.params;
-    const db = await openDatabase();
-    const data = await db.run(`
-        DELETE FROM activities 
-        WHERE id = ?
-    `, [id])
-    db.close();
-    response.send({
-        id,
-        message: `Atividade [${id}] removida com sucesso`
-    });
-});
-
-app.get('/api/activities', async (request, response) => {
-    
-    const db = await openDatabase();
-    const activities = await db.all(`
-        SELECT * FROM activities
-    `)
-    db.close();
-    response.send(activities)
-
-});
+app.post('/api/activities/checkin', checkIn);
+app.put('/api/activities/checkout', checkOut);
+app.delete('/api/activities/:id', deleteActivity);
+app.get('/api/activities', listActivities);
 
 app.listen(8000, () => {
     console.log("Servidor rodando na porta 8000");

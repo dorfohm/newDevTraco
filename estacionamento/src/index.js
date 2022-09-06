160eda2d2252e052866416a6bf357eb316babd83
dorfohm <dorfohm@hotmail.com>
2022-09-06 17:14:24 -0300

    elaborado CRUD de activities

--- estacionamento/src/index.js
+++ estacionamento/src/index.js
@@ -49,7 +49,7 @@ app.put('/api/vehicles/:id', async(request, response) => {
 
     const vehicle = await db.get(`
     SELECT * FROM vehicles WHERE id = ? 
-`, [id]);
+    `, [id]);
     if(vehicle) {
         const data = await db.run(`
         UPDATE vehicles
@@ -89,6 +89,113 @@ app.delete('/api/vehicles/:id', async(request, response) => {
         id,
         message: `Veículo [${id}] removido com sucesso`
     });
+});
+
+//Endpoints de activities
+app.post('/api/activities/checkin', async (request, response) => {
+    const { label } = request.body; 
+
+    const db = await openDatabase();
+
+    const vehicle = await db.get(`
+    SELECT * FROM vehicles WHERE label = ? 
+    `, [label]);
+
+    if (vehicle) {
+        const checkinAt = (new Date()).getTime();
+        const data = await db.run(`
+        INSERT INTO activities (vehicle_id, checkin_at)
+        VALUES (?, ?)
+        `, [vehicle.id, checkinAt]);
+        db.close();
+        response.send({
+        vehicle_id: vehicle.id,
+        checkin_at: checkinAt,
+        message: `Veículo [${vehicle.label}] entrou no estacionamento`
+        });
+        return;
+    }
+
+    db.close();
+    response.send({
+        message: `Veículo [${label}] não cadastrado`
+    });
+});
+
+app.put('/api/activities/checkout', async (request, response) => {
+    const { label, price } = request.body; 
+
+    const db = await openDatabase();
+
+    const vehicle = await db.get(`
+    SELECT * FROM vehicles WHERE label = ? 
+    `, [label]);
+
+    if (vehicle) {
+
+        const activityOpen = await db.get(`
+            SELECT * 
+            FROM activities 
+            WHERE vehicle_id = ? 
+            AND checkout_at IS NULL
+        `, [vehicle.id]);
+
+        if  (activityOpen) {
+            const checkoutAt = (new Date()).getTime();
+            const data = await db.run(`
+            UPDATE activities 
+            SET checkout_at = ?,
+                price = ?
+            WHERE id = ?
+            `, [checkoutAt, price, activityOpen.id]);
+
+            db.close();
+            response.send({
+            vehicle_id: vehicle.id,
+            checkout_at: checkoutAt,
+            price: price,
+            message: `Veículo [${vehicle.label}] saiu do estacionamento`
+            });
+            return;
+        }
+
+        db.close();
+        response.send({
+        message: `Veículo [${label}] não realizou nenhum check-in`
+        });
+        return;
+    
+    }
+
+    db.close();
+    response.send({
+        message: `Veículo [${label}] não cadastrado`
+    });
+});
+
+app.delete('/api/activities/:id', async (request, response) => {
+    const { id } = request.params;
+    const db = await openDatabase();
+    const data = await db.run(`
+        DELETE FROM activities 
+        WHERE id = ?
+    `, [id])
+    db.close();
+    response.send({
+        id,
+        message: `Atividade [${id}] removida com sucesso`
+    });
+});
+
+app.get('/api/activities', async (request, response) => {
+    
+    const db = await openDatabase();
+    const activities = await db.all(`
+        SELECT * FROM activities
+    `)
+    db.close();
+    response.send(activities)
+
 });
 
 app.listen(8000, () => {

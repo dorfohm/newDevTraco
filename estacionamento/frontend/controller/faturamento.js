f96838d4ac2a139aad1f212d635417765946c767
dorfohm <dorfohm@hotmail.com>
2022-09-15 12:41:52 -0300

    Programa finalizado

--- estacionamento/frontend/controller/faturamento.js
+++ estacionamento/frontend/controller/faturamento.js
@@ -13,13 +13,123 @@ export const FaturamentoComponent = () => {
             }
         });
         gerarObjetosDatas();
+        gerarFaturamento();
     })
-}
+    
+    let datasFiltradas = [];
 
-const datasBrutas = [];
+    const gerarObjetosDatas = () => {
+        const datasBrutas = [];
+        objetoFaturamento.forEach((element) => {
+            datasBrutas.push(dataConvert(element.checkout_at))
+        })
+        datasFiltradas = new Set(datasBrutas);
+        console.log(datasFiltradas);
+    }
 
-const gerarObjetosDatas = () => {
-    objetoFaturamento.forEach((element) => {
-        datasBrutas.push(element.checkout_at)
+    const dataConvert = (tempo)  => {
+        const data = new Date(tempo).getDate()
+        return data;
+    }
+
+    const gerarFaturamento = () => {
+        let valor = {
+            contador: 0,
+            total: 0
+        } 
+        objetoFaturamento.forEach((element) => {
+            if(typeof element.price == "number"){
+                valor.contador++;
+                valor.total += element.price;
+            }
+        })
+        criarNovaLinha(valor);
+        criarOpcoes(datasFiltradas);
+    }
+
+    const tabela = document.getElementById("tbody");
+
+    const criarNovaLinha = (valor) => {     
+        const linhaNovo = document.createElement("tr");
+        const dadosHtml = `
+        <td id = "qtd">${valor.contador}</td>
+        <td id = "total">R$ ${valor.total.toFixed(2)}</td>
+        <td><select id = "datas"></select></td>
+        `
+        linhaNovo.innerHTML = dadosHtml;
+        renderGrafico();
+        return tabela.appendChild(linhaNovo);
+    }
+
+    const criarOpcoes = (datas) => {
+        const select = document.getElementById('datas');
+        datas.forEach((element) => {
+            const option = new Option(element,element)
+            select.add(option);
+        })
+    }
+
+    tabela.addEventListener('click', (event) => {
+        if(event.path[0].id == 'datas'){
+            filtrarPorDatas(event)
+        }
     })
+
+    const filtrarPorDatas = (event) => {
+        const dia = event.path[0].value;
+        let valor = {
+            contador: 0,
+            total: 0
+        } 
+        objetoFaturamento.forEach((element) => {
+            if(dataConvert(element.checkout_at) == dia){
+                valor.contador++;
+                valor.total += element.price;
+            }
+        })
+        atualizarHtml(valor)
+    }
+
+    const atualizarHtml = (valor) => {
+        document.getElementById('qtd').innerText = valor.contador;
+        document.getElementById('total').innerText = `R$ ${valor.total.toFixed(2)}`;
+    }
+
+    let pieChartArray = [['Dia', 'Faturamento', ]];
+
+    const filtrarFaturamentoPorDatas = (datas) => {
+        let valor = {
+            contador: 0,
+            total: 0
+        } 
+        datas.forEach((elementDatas) => {
+            valor.total = 0;
+            objetoFaturamento.forEach((element) => {
+                if(dataConvert(element.checkout_at) == elementDatas){
+                    valor.contador++;
+                    valor.total += element.price;
+                }
+            })
+            pieChartArray.push([`${elementDatas}`, + valor.total.toFixed(2)])
+        })  
+    }
+
+    function renderGrafico() {
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+        function drawChart() {
+
+            filtrarFaturamentoPorDatas(datasFiltradas);
+
+            var data = google.visualization.arrayToDataTable(pieChartArray);
+
+            var options = {
+                is3D:true
+            };
+
+            var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+
+            chart.draw(data, options);
+        }
+    }
 }
